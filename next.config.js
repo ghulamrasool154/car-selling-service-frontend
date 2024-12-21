d5728bd38e791251095d366bc2e4475b28ef1389
Ghulam Rasool <80417750+ghulamrasool154@users.noreply.github.com>
2024-12-21 19:07:20 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    api_url: "https://car-selling-service-api-kappa.vercel.app/api/v1/",
+    api_url: "/api/v1/",
   },
   images: {
     remotePatterns: [
